f07d9c958a6d605f90f65024f1689ff9754acfd3
Teun Verhaert <teun.verhaert1@gmail.com>
2018-10-12 09:31:29 +0200

    Prototype done

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,50 +1,113 @@
-pragma solidity 0.4.20;
+pragma solidity ^0.4.24;
 
 contract Election {
+    struct Location {
+      uint id;
+      uint validAddressAmount;
+      address[] validAddresses;
+    }
+
+    struct Party {
+      uint id;
+      string name;
+      uint voteCount;
+    }
+
     // Model a Candidate
     struct Candidate {
         uint id;
+        uint partyId;
         string name;
         uint voteCount;
     }
 
     // Store accounts that have voted
-    mapping(address => bool) public voters;
+    mapping(address => uint) public votersParty;
+    mapping(address => uint[]) public votersCandidates;
+    mapping(address => uint) public votersCandidatesCount;
+    mapping(address => bool) public voted;
     // Store Candidates
     // Fetch Candidate
     mapping(uint => Candidate) public candidates;
     // Store Candidates Count
     uint public candidatesCount;
 
+    mapping (uint => Party) public parties;
+    uint public partyCount;
+
+    mapping (uint => Location) public locations;
+    uint public locationCount;
+
     // voted event
     event votedEvent (
         uint indexed _candidateId
     );
 
-    function Election () public {
-        addCandidate("Candidate 1");
-        addCandidate("Candidate 2");
+    constructor() {
+        address[] memory a = new address[](2);
+        a[0] = 0x0260C4873d8C3d9ABe737450516D266dA391e2f5;
+        a[1] = 0xa00e6969C7B59122B76c0fFcb8C87A1809FEfF3F;
+        addLocation(a);
+        addParty("SP.A");
+        addParty("N-VA");
+        addParty("CD&V");
+        addCandidate("BEELS Jinnih",1);
+        addCandidate("MEEUWS Tom",1);
+        addCandidate("TURAN Güler",1);
+        addCandidate("DE WEVER Bart",2);
+        addCandidate("DE RIDDER Annick",2);
+        addCandidate("AIT DAOUD Nabilla",2);
+        addCandidate("PEETERS Kris",3);
+        addCandidate("BASTIAENS Caroline",3);
+        addCandidate("LANJRI Nahima",3);
     }
 
-    function addCandidate (string _name) private {
+    function addCandidate (string _name, uint _partyId) private {
         candidatesCount ++;
-        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+        candidates[candidatesCount] = Candidate(candidatesCount, _partyId, _name, 0);
     }
 
-    function vote (uint _candidateId) public {
-        // require that they haven't voted before
-        require(!voters[msg.sender]);
+    function addParty(string _name) {
+        partyCount++;
+        parties[partyCount] = Party(partyCount, _name, 0);
+    }
 
-        // require a valid candidate
-        require(_candidateId > 0 && _candidateId <= candidatesCount);
+    function addLocation(address[] _validAddresses) {
+        locationCount++;
+        locations[locationCount] = Location(locationCount, _validAddresses.length, _validAddresses);
+    }
+
+    function vote (uint _partyId, uint[] _candidateIds, uint _locationId) public {
+        // require that they haven't voted before
+        require(!voted[msg.sender]);
+
+        bool found = false;
+        for (uint j=0; j<locations[_locationId].validAddressAmount; j++) {
+          if (locations[_locationId].validAddresses[j] == msg.sender) {
+            found = true;
+            break;
+          }
+        }
+
+        require(found);
 
         // record that voter has voted
-        voters[msg.sender] = true;
+        voted[msg.sender] = true;
+
+        parties[_partyId].voteCount++;
 
         // update candidate vote Count
-        candidates[_candidateId].voteCount ++;
+        for (uint i=0; i<_candidateIds.length; i++) {
+          uint id = _candidateIds[i];
+          require(id > 0 && id <= candidatesCount && candidates[id].partyId == _partyId);
+          candidates[id].voteCount ++;
+          votersCandidates[msg.sender].push(id);
+          votersCandidatesCount[msg.sender] = votersCandidatesCount[msg.sender] + 1;
+        }
+
+        votersParty[msg.sender] = _partyId;
 
         // trigger voted event
-        votedEvent(_candidateId);
+        emit votedEvent(_partyId);
     }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,14 @@ App = {
   contracts: {},
   account: '0x0',
   hasVoted: false,
+  parties: [],
+  candidates: [],
+  partyCount: 0,
+  candidatesCount: 0,
+  location: {
+    id: 1,
+    name: "Kerkstraat 5"
+  },
 
   init: function() {
     return App.initWeb3();
@@ -52,10 +60,81 @@ App = {
     });
   },
 
+  getData: function() {
+    /*var electionInstance;
+    App.contracts.Election.deployed().then(function(instance) {
+      electionInstance = instance;
+      return electionInstance.partyCount();
+    }).then(function(partyCount) {
+      App.partyCount = partyCount;
+      for (let i = 0; i<=partyCount; i++) {
+        electionInstance.parties(i).then(function(party) {
+          App.parties.push(party);
+          App.checkData();
+        })
+      }
+      return electionInstance.candidatesCount();
+    }).then(function(candidatesCount) {
+      App.candidatesCount = candidatesCount;
+      for (let i = 0; i<=candidatesCount; i++) {
+        electionInstance.candidates(i).then(function(candidate) {
+          App.candidates.push(candidate);
+          App.checkData();
+        })
+      }
+    })*/
+    App.parties = [{
+      id: 1,
+      name: "SP.A",
+      voteCount: 0
+    }, {
+      id: 2,
+      name: "N-VA",
+      voteCount: 0
+    }, {
+      id: 3,
+      name: "CD&V",
+      voteCount: 0
+    }];
+    App.addCandidate(1, "BEELS Jinnih");
+    App.addCandidate(1, "MEEUWS Tom");
+    App.addCandidate(1, "TURAN Güler");
+    App.addCandidate(2, "DE WEVER Bart");
+    App.addCandidate(2, "DE RIDDER Annick");
+    App.addCandidate(2, "AIT DAOUD Nabilla");
+    App.addCandidate(3, "PEETERS Kris");
+    App.addCandidate(3, "BASTIAENS Caroline");
+    App.addCandidate(3, "LANJRI Nahima");
+    console.log(App.parties);
+    console.log(App.candidates);
+  },
+
+  addCandidate: function(partyId, name) {
+    App.candidates.push({
+      id: App.candidates.length+1,
+      partyId: partyId,
+      name: name,
+      voteCount: 0
+    })
+  },
+
+  checkData: function() {
+    console.log(App.parties.length, "party length");
+    console.log(App.partyCount, "party count");
+    if (App.parties.length == App.partyCount && App.candidates.length == App.candidatesCount) {
+      console.log(App.parties, "parties");
+      console.log(App.candidates, "candidates");
+    }
+  },
+
   render: function() {
+    getAllVotes();
+    App.getData();
     var electionInstance;
     var loader = $("#loader");
     var content = $("#content");
+
+    $('#location').html(App.location.name);
 
     loader.show();
     content.hide();
@@ -71,30 +150,41 @@ App = {
     // Load contract data
     App.contracts.Election.deployed().then(function(instance) {
       electionInstance = instance;
+      console.log(electionInstance);
       return electionInstance.candidatesCount();
     }).then(function(candidatesCount) {
+      console.log(candidatesCount, "candidatesCount");
       var candidatesResults = $("#candidatesResults");
       candidatesResults.empty();
 
-      var candidatesSelect = $('#candidatesSelect');
-      candidatesSelect.empty();
+      for(let i=0;i<App.parties.length;i++) {
+        var headerTemplate = "<tr><td><input type='checkbox' onclick='partyClick(value)'  value=" + i + " id='"+ i +"'></td><td></td><td>" + App.parties[i].name + "</td><td></td></tr>";
+          candidatesResults.append(headerTemplate);
+        for(let j=i*3;j<i*3+3;j++) {
+          var template = "<tr><td><input type='checkbox' onclick='buttonClick(value)'  value=" + App.candidates[j].id + " id='can" + App.candidates[j].id +"'></td><td>" + App.candidates[j].id + "</td><td>" + App.candidates[j].name + "</td><td>" + App.parties[i].name +"</td></tr>";
+            candidatesResults.append(template);
+        }
+        var divider = "<tr><td></td><td></td><td></td><td></td></tr>"
+        candidatesResults.append(divider);
+      }
 
-      for (var i = 1; i <= candidatesCount; i++) {
+      /*for (var i = 1; i <= candidatesCount; i++) {
         electionInstance.candidates(i).then(function(candidate) {
           var id = candidate[0];
-          var name = candidate[1];
-          var voteCount = candidate[2];
+          var partyId = candidate[1];
+          var name = candidate[2];
+          var voteCount = candidate[3];
 
           // Render candidate Result
-          var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
+          var candidateTemplate = "<tr><td><input type='checkbox' onclick='buttonClick(value)'  value=" + id + "></td><td>" + id + "</td><td>" + name + "</td><td>" + partyId +"</td><tr>"
           candidatesResults.append(candidateTemplate);
 
           // Render candidate ballot option
           var candidateOption = "<option value='" + id + "' >" + name + "</ option>"
           candidatesSelect.append(candidateOption);
         });
-      }
-      return electionInstance.voters(App.account);
+      }*/
+      return electionInstance.voted(App.account);
     }).then(function(hasVoted) {
       // Do not allow a user to vote
       if(hasVoted) {
@@ -104,21 +194,11 @@ App = {
       content.show();
     }).catch(function(error) {
       console.warn(error);
-    });
-  },
-
-  castVote: function() {
-    var candidateId = $('#candidatesSelect').val();
-    App.contracts.Election.deployed().then(function(instance) {
-      return instance.vote(candidateId, { from: App.account });
-    }).then(function(result) {
-      // Wait for votes to update
-      $("#content").hide();
-      $("#loader").show();
-    }).catch(function(err) {
-      console.error(err);
+      loader.hide();
+      content.show();
     });
   }
+
 };
 
 $(function() {
@@ -126,3 +206,139 @@ $(function() {
     App.init();
   });
 });
+
+function getVote() {
+  var accountAddress = "0x"+$('#naam').val();
+  var electionInstance;
+  App.contracts.Election.deployed().then(function(instance) {
+    electionInstance = instance;
+    return instance.votersParty(accountAddress);
+  }).then(function(partyId) {
+    electionInstance.parties(partyId).then(function(party) {
+      console.log(party);
+      $("#partij").html(party[1]);
+    })
+  });
+  App.contracts.Election.deployed().then(function(instance) {
+    return instance.votersCandidatesCount(accountAddress);
+  }).then(function(votedCount) {
+    console.log(votedCount, "voted count");
+    $("#partijLeden").empty();
+    for (let i=0; i<votedCount; i++) {
+      electionInstance.votersCandidates(accountAddress, i).then(function(res) {
+        return electionInstance.candidates(res);
+      }).then(function(candidate) {
+        console.log(candidate);
+        $("#partijLeden").append(candidate[2]+"<br>");
+      })
+    }
+  });
+}
+
+var arr = [];
+var selectedParty = -1;
+function buttonClick(value) {
+  value = Number(value);
+  var selector = "#can" + value;
+  console.log(selector);
+  if (arr.includes(value)) {
+    arr.splice(arr.indexOf(value), 1);
+  } else {
+    console.log(App.candidates[value-1].partyId, "candidate party id");
+    if (arr.length > 0 && App.candidates[value-1].partyId != selectedParty) {
+        $(selector).prop('checked', false);
+        return;
+    }
+    arr.push(value);
+    var str = '#'+(App.candidates[value-1].partyId-1);
+    $(str).attr('checked','checked');
+    selectedParty = App.candidates[value-1].partyId;
+  }
+  console.log(arr);
+  console.log(selectedParty);
+}
+
+function partyClick(value) {
+  selectedParty = value;
+  console.log(value);
+}
+
+function castVote() {
+  App.contracts.Election.deployed().then(function(instance) {
+    console.log({
+      party: selectedParty,
+      candidates: arr,
+      location: App.location.id
+    });
+    return instance.vote(selectedParty, arr, App.location.id, { from: App.account })
+  }).then(function(result) {
+    // Wait for votes to update
+    $("#content").hide();
+    $("#loader").show();
+  }).catch(function(err) {
+    console.error(err);
+  });
+}
+
+var totalVotes = 0;
+parties = [];
+var dataChart = [];
+chartLoaded = false;
+
+function getAllVotes() {
+  totalVotes = 0;
+  var electionInstance;
+  App.contracts.Election.deployed().then(function(instance) {
+    electionInstance = instance;
+    return instance.partyCount();
+  }).then(function(partyCount) {
+    for(let i=1;i<=partyCount;i++) {
+      electionInstance.parties(i).then(function(party) {
+        totalVotes += Number(party[2]);
+        parties.push({
+          name: party[1],
+          votes: Number(party[2])
+        })
+        try {
+          calcChart();
+        } catch(e) {
+          console.log(e);
+        }
+        console.log(parties);
+      })
+    }
+  });
+}
+
+function calcChart() {
+  if (chartLoaded) {
+    return;
+  }
+  dataChart = [];
+  for (let i =0;i<parties.length;i++) {
+    dataChart.push({
+      politicalParty: parties[i].name,
+      votes: parties[i].votes / totalVotes * 100,
+      color: getColor(parties[i].name)
+    })
+  }
+  console.log(dataChart, "datachart");
+  if (parties.length == 3) {
+    chartLoaded = true;
+  }
+  try {
+    createChart(dataChart);
+  } catch(e) {
+    console.log(e);
+  }
+}
+
+function getColor(name) {
+  if (name === "SP.A") {
+    return "#e4010b";
+  } else if (name==="N-VA") {
+    return "#fbbc19";
+  } else {
+    return "#f79706";
+  }
+}
